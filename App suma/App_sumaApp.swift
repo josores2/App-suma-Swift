3888f9afda52518e8c90aad5dd66854447bb9cad
jose <josesorliquart@gmail.com>
2024-03-22 18:27:14 +0100

    Initial Commit

---
+++ App suma/App_sumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_sumaApp.swift
+//  App suma
+//
+//  Created by Jose on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct App_sumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App suma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App suma
+//
+//  Created by Jose on 22/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
